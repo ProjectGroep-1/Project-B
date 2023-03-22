ed3b0eeb8d72c63c56b69daa21bde9eacfb881ea
ArmanXDDD <113505559+ArmanXDDD@users.noreply.github.com>
2023-03-22 14:39:13 +0100

    Update Menu.cs

--- Presentation/Menu.cs
+++ Presentation/Menu.cs
@@ -6,33 +6,37 @@ static class Menu
     //You could edit this to show different menus depending on the user's role
     static public void Start()
     {
-        Console.WriteLine("Main menu" + "\n");
-        Console.WriteLine("1: Current menu" + "\n" + "2: Information about our restaurant" + "\n" + "3: Your reservation" + "\n" + "4: Contact" + "\n");
+        while (true)
+        {
+            Console.WriteLine("Main menu" + "\n");
+            Console.WriteLine("1: Current menu" + "\n" + "2: Information about our restaurant" + "\n" + "3: Your reservation" + "\n" + "4: Contact" + "\n");
 
-        string message = "Under maintenance";
-
-        string input = Console.ReadLine();
-        if (input == "1")
-        {
-            Console.WriteLine(message);
-        }
-        else if (input == "2")
-        {
-            Console.WriteLine(message);
-        }
-        else if (input == "3")
-        {
-            Console.WriteLine(message);
-        }
-        else if (input == "4")
-        {
-            Console.WriteLine(message);
-        }
-        else
-        {
+            string message = "Under maintenance";
+            string input = Console.ReadLine();
+            
+            if (input == "1")
+            {
+                Console.WriteLine(message);
+                break;
+            }
+            if (input == "2")
+            {
+                Console.WriteLine(message);
+                break;
+            }
+            if (input == "3")
+            {
+                Console.WriteLine(message);
+                break;
+            }
+            if (input == "4")
+            {
+                Console.WriteLine(message);
+                break;
+            }
             Console.WriteLine("Invalid input");
-            Start();
         }
+
 
     }
 }
