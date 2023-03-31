6dd9094d8988da18ac308de7a9e3fac7d56a8aa3
Jochem van Staalduinen <114492359+jochemvs@users.noreply.github.com>
2023-03-31 12:21:45 +0200

    Changed Contact information

--- Presentation/Menu.cs
+++ Presentation/Menu.cs
@@ -129,10 +129,41 @@ static class Menu
             }
             else if (input == "4")
             {
-                Console.Clear();
-                AccountsAccess.GetContactInformation();
-                Console.ReadKey(true);
-                
+                Console.WriteLine("1: View contact information"+ "\n" + "2: Edit contact information");
+                TextFile ContactFile  = new TextFile(@"DataSources\ContactFile.txt");
+                string ContactInput = Console.ReadLine();
+                int variable = 0;
+                int.TryParse(ContactInput, out variable);
+                switch (variable){
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine(ContactFile.Read());
+                        Console.ReadKey(true);
+                        break;
+                    case 2: 
+                        Console.Clear();
+                        Console.WriteLine("1: Edit Phone number"+ "\n" + "2: Edit Adress" + "\n" + "3: Edit E-mail");
+                        string contactinput_2 = Console.ReadLine();
+                        if(contactinput_2 == "1"){
+                            Console.WriteLine("What is your new phone number?");
+                            string new_number = $"Phone number: {Console.ReadLine()}";
+                             ContactFile.ChangeValueById(0, new_number);}
+
+                        else if(contactinput_2 == "2"){
+                            Console.WriteLine("What is your new Adress?");
+                            string new_adress = $"Adress: {Console.ReadLine()}";
+                             ContactFile.ChangeValueById(1, new_adress);}
+
+                        else if(contactinput_2 == "3"){
+                            Console.WriteLine("What is your new Email adress?");
+                            string new_email = $"Email : {Console.ReadLine()}";
+                             ContactFile.ChangeValueById(2, new_email);}
+                         else{
+                            Console.WriteLine("Invalid input");
+                            Console.ReadKey(true);
+                         }
+                    break;
+                }
             }
             else{
                 Console.WriteLine("Invalid input");
@@ -143,4 +174,5 @@ static class Menu
 
 
     }
-}
+    }
+
