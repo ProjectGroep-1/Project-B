311192828a6a7a3b96c1af2519464b4d06b13123
Jochem van Staalduinen <jochemvanstaalduinen2@gmail.com>
2023-04-26 12:27:23 +0200

    Fixed contactinformation editing, added try-catch

--- ShowCase/Presentation/Menu.cs
+++ ShowCase/Presentation/Menu.cs
@@ -113,14 +113,13 @@ static class Menu
                         FoodMenuFunctions.GetSearchOptions();
                         break;
                     case 6:
-                    Console.WriteLine("Under Maintenance" + "\n" + "Press any key");
-                    string PressEnter = Console.ReadLine();
-
-                    /* Console.WriteLine("1: View contact information"+ "\n" + "2: Edit contact information");
+                    Console.WriteLine("Under Maintenance");
+                    TextFile ContactFile = new TextFile("DataSources\\ContactFile.txt");
+                    Console.WriteLine("1: View contact information"+ "\n" + "2: Edit contact information");
                     string ContactInput = Console.ReadLine();
-                    int variable = 0;
-                    int.TryParse(ContactInput, out variable);
-                    switch (variable){
+                    int switcher = 0;
+                    int.TryParse(ContactInput, out switcher);
+                    switch (switcher){
                         case 1:
                             Console.Clear();
                             Console.WriteLine(ContactFile.Read());
@@ -131,25 +130,48 @@ static class Menu
                             Console.WriteLine("1: Edit Phone number"+ "\n" + "2: Edit Adress" + "\n" + "3: Edit E-mail");
                             string contactinput_2 = Console.ReadLine();
                             if(contactinput_2 == "1"){
+                                try{
                                 Console.WriteLine("What is your new phone number?");
                                 string new_number = $"Phone number: {Console.ReadLine()}";
                                 ContactFile.ChangeValueById(0, new_number);}
+                                catch (IOException e) {
+                                Console.WriteLine("Error occurred while editing file: " + e.Message);
+                                }}
 
                             else if(contactinput_2 == "2"){
+                                try{
                                 Console.WriteLine("What is your new Adress?");
                                 string new_adress = $"Adress: {Console.ReadLine()}";
                                 ContactFile.ChangeValueById(1, new_adress);}
+                                catch (IOException e) {
+                                Console.WriteLine("Error occurred while editing file: " + e.Message);
+                                }}
 
                             else if(contactinput_2 == "3"){
+                                try{
                                 Console.WriteLine("What is your new Email adress?");
                                 string new_email = $"Email : {Console.ReadLine()}";
                                 ContactFile.ChangeValueById(2, new_email);}
+                                catch (IOException e) {
+                                Console.WriteLine("Error occurred while editing file: " + e.Message);
+                                }}
                             else{
                                 Console.WriteLine("Invalid input");
                                 Console.ReadKey(true);
-                            } */
+                            } 
 
+                        break; 
+                    default:
+                        Console.WriteLine("You've entered an invalid input, try again");
                         break;
+                    
+                }
+                break;
+                
+            
+            
+
+                        
                     
                     case 7:
                         TableAccess.ShowCapacity();
